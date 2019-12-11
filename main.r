67708b6e5dc49a64451c1231ebd7fbef4df6b766
OtakoidTony <36785562+OtakoidTony@users.noreply.github.com>
2019-12-11 23:42:42 +0900

    Update main.r

--- main.r
+++ main.r
@@ -2,15 +2,27 @@ BaseUrl <- "https://discordapp.com/api"
 token <- "your token"
 
 
+# TODO: Channel management | Create Delete DM Edit History Info Permission
+# TODO: Edit Profile       | Edit_Profile
+# TODO: Invites            | Create Delete Info Join
+# TODO: Message management | Send Send_File Edit Delete	
+# TODO: Role management    | Create Delete Edit Info	
+# TODO: Server management  | Ban Ban_List Create Delete Edit Info Kick Unban Change_Owner
+# TODO: User management    | Voice_Move
+
+
+# Get Gateway
 getGateway <- function(){
   r <- GET("https://discordapp.com/api/v6/gateway")
 }
 
+# Get Gateway Bot
 getGatewayBot <- function(){
   key <- paste0("Bot ", token)
   r <- GET("https://discordapp.com/api/v6/gateway/bot", add_headers(Authorization=key))
 }
 
+# Message management | Send
 sendMessage <- function(channel_id, message){
   key <- paste0("Bot ", token)
   Url <- paste0("https://discordapp.com/api/v6/channels/")
@@ -37,3 +49,4 @@ getChannel <- function(channel_id){
   r <- GET(Url, add_headers(Authorization=key))
   return(r)
 }
+
