999bedf687070438ccefeec824f7080b8cf19d38
OtakoidTony <36785562+OtakoidTony@users.noreply.github.com>
2019-12-11 21:35:29 +0900

    Update main.r

--- main.r
+++ main.r
@@ -1,6 +1,11 @@
 BaseUrl <- "https://discordapp.com/api"
 token <- "your token"
 
+
+getGateway <- function(){
+  r <- GET("https://discordapp.com/api/v6/gateway")
+}
+
 getGatewayBot <- function(){
   key <- paste0("Bot ", token)
   r <- GET("https://discordapp.com/api/v6/gateway/bot", add_headers(Authorization=key))
@@ -13,5 +18,22 @@ sendMessage <- function(channel_id, message){
   Url <- paste0(Url, "/messages")
   body <- list(tts=FALSE, content=message)
   r <- POST(Url, add_headers(Authorization=key), set_cookies(content(getGatewayBot())$cookies), body = body, encode = "json")
+  return(r)
+}
+
+getMessage <- function(channel_id){
+  key <- paste0("Bot ", token)
+  Url <- paste0("https://discordapp.com/api/v6/channels/")
+  Url <- paste0(Url, channel_id)
+  Url <- paste0(Url, "/messages")
+  r <- GET(Url, add_headers(Authorization=key))
+  return(r)
+}
+
+getChannel <- function(channel_id){
+  key <- paste0("Bot ", token)
+  Url <- paste0("https://discordapp.com/api/v6/channels/")
+  Url <- paste0(Url, channel_id)
+  r <- GET(Url, add_headers(Authorization=key))
   return(r)
 }
